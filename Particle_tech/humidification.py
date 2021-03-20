c6db4a495f6768656cbc53a8222b5e103ab9fd29
DewaldMare <69504854+DewaldMare@users.noreply.github.com>
2021-03-20 09:34:59 +0200

    v0

--- Particle_tech/humidification.py
+++ Particle_tech/humidification.py
@@ -1,8 +1,41 @@
+'''
+This module can be used to perform typical Humidification calculations.
+Functions available in this module:
+- humid: Calculate the humidity using the partial pressure of the compound that can condense, the total pressure and the molar masses.
+                                                H = (P / (Pt - P)) * (MMa / MMb)                                (1)
+- Pa: Calculate the partial pressure of the compound using equation (1) and a inital guess pressure to solve for pressure.
+- Vh: Calculate the specific humid volume V'.
+                                                V = (1 / MMb + H / MMa) * ((R * T) / Pt)                        (2)
+-enthalpy: Calculate the enthalpy of the system. Current version only solves for water and toluene and Cp values needs to be inserted manually.
+                                                enth = S * (T - Tref) + λ_w * H                                 (3)
+-percH: Calculate the percentage humidity. The ratio of the humidty to the saturated humidity.
+                                                %H = H / Ho                                                     (4)
+-relH: Calculate the relative humidity. The ratio of the partial pressure of the vapour to the saturated humidity.
+                                                Hr = Pa/Pa_sat                                                  (5)
+
+Nomenclature:
+Symbol      Name                                                Units
+- P         Pressure                                            (kPa)
+- Pt        Total pressure                                      (kPa)
+- MMa       Molar mass of component that will condense          (kg/kmol)
+- MMb       Molar mass of component that will condense          (kg/kmol)
+- H         Humidity                                            (kg/kg)
+- T         Temperature                                         (K)
+- pa        Partial pressure of component that will condense    (kPa)
+- psat      Saturated pressure of component that will condense  (kPa)
+- dec       Decimal places                                      (-)
+
+This module works well in combination with the Antoine module.
+Created by Dewald Mare.
+
+'''
+
 def humid(P, Pt, MMa, MMb):
     H = (P / (Pt - P)) * (MMa / MMb)
     return H
 
 λ_water = lambda T: 2500 - 2.546 * T #in degC
+λ_tol = lambda T: 406.65 - 0.752 * T #in degC
 
 def Pa(H, Pt, MMa, MMb, guessP):
     from scipy.optimize import fsolve
@@ -19,11 +52,14 @@ def Vh(MMa, MMb, H, Pt, T):
     V = (1 / MMb + H / MMa) * ((R * T) / Pt)
     return V
 
-def enthalpy(Cpa, Cpb, H, T):
-    λ_w = λ(T)
+def enthalpy(name, Cpa, Cpb, H, T):
+    if name == "Water":
+        λ = λ_water(T - 273.15)
+    elif name == "Toluene":
+        λ = λ_tol(T - 273.15)
     S = Cpb + Cpa * H
     Tref = 273.15 #K
-    enth = S * (T - Tref) + λ_w * H
+    enth = S * (T - Tref) + λ * H
     return enth
 
 def percH(pa, psat, T, Pt, MMa, MMb, dec):
