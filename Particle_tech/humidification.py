98c347e1b280df513ca6e67415e96a41c43be1af
DewaldMare <69504854+DewaldMare@users.noreply.github.com>
2021-03-20 10:28:39 +0200

    v2

--- Particle_tech/humidification.py
+++ Particle_tech/humidification.py
@@ -2,40 +2,40 @@
 This module can be used to perform typical Humidification calculations.
 Functions available in this module:
 - humid: Calculate the humidity using the partial pressure of the compound that can condense, the total pressure and the molar masses.
-                                                H = (P / (Pt - P)) * (MMa / MMb)                                (1)
+                                                H = (pa / (Pt - pa)) * (MMa / MMb)                              (1)
 - Pa: Calculate the partial pressure of the compound using equation (1) and a inital guess pressure to solve for pressure.
 - Vh: Calculate the specific humid volume V'.
                                                 V = (1 / MMb + H / MMa) * ((R * T) / Pt)                        (2)
+- λ_water: Calulate the latent heat of vaporization of water. Temperature is in degrees C
+                                                λ_water = 2500 - 2.546 * T                                      (3)
+- λ_tol: Calulate the latent heat of vaporization of toluene. Temperature is in degrees C
+                                                λ_tol = 406.65 - 0.752 * T                                      (4)
 - enthalpy: Calculate the enthalpy of the system. Current version only solves for water and toluene and Cp values needs to be inserted manually.
-                                                enth = S * (T - Tref) + λ_w * H                                 (3)
+                                                enth = S * (T - Tref) + λ_w * H                                 (5)
 - percH: Calculate the percentage humidity. The ratio of the humidty to the saturated humidity.
-                                                %H = H / Ho                                                     (4)
+                                                %H = H / Ho                                                     (6)
 - relH: Calculate the relative humidity. The ratio of the partial pressure of the vapour to the saturated humidity.
-                                                Hr = Pa/Pa_sat                                                  (5)
+                                                Hr = pa/pa_sat                                                  (7)
 
 Nomenclature:
-Symbol      Name                                                Units
-- P         Pressure                                            (kPa)
-- Pt        Total pressure                                      (kPa)
-- MMa       Molar mass of component that will condense          (kg/kmol)
-- MMb       Molar mass of component that will condense          (kg/kmol)
-- H         Humidity                                            (kg/kg)
-- T         Temperature                                         (K)
-- pa        Partial pressure of component that will condense    (kPa)
-- psat      Saturated pressure of component that will condense  (kPa)
-- dec       Decimal places                                      (-)
+Symbol      Name                                                    Units
+- pa        Partial pressure of component that will condense        (kPa)
+- Pt        Total pressure                                          (kPa)
+- MMa       Molar mass of component that will condense              (kg/kmol)
+- MMb       Molar mass of component that will condense              (kg/kmol)
+- H         Humidity                                                (kg/kg)
+- T         Temperature                                             (K)
+- pa_sat    Saturated pressure of component that will condense      (kPa)
+- dec       Decimal places                                          (-)
 
 This module works well in combination with the Antoine module.
 Created by Dewald Mare.
 
 '''
 
-def humid(P, Pt, MMa, MMb):
-    H = (P / (Pt - P)) * (MMa / MMb)
+def humid(pa, Pt, MMa, MMb):
+    H = (pa / (Pt - pa)) * (MMa / MMb)
     return H
-
-λ_water = lambda T: 2500 - 2.546 * T #in degC
-λ_tol = lambda T: 406.65 - 0.752 * T #in degC
 
 def Pa(H, Pt, MMa, MMb, guessP):
     from scipy.optimize import fsolve
@@ -51,6 +51,9 @@ def Vh(MMa, MMb, H, Pt, T):
     R = 8.3145 #kJ/kmolK
     V = (1 / MMb + H / MMa) * ((R * T) / Pt)
     return V
+
+λ_water = lambda T: 2500 - 2.546 * T #in degC
+λ_tol = lambda T: 406.65 - 0.752 * T #in degC
 
 def enthalpy(name, Cpa, Cpb, H, T):
     if name == "Water":
