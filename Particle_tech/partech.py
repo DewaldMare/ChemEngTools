9c9538470136cf5fd57d7868ac12ba466f2ae43d
DewaldMare <69504854+DewaldMare@users.noreply.github.com>
2021-03-22 09:09:45 +0200

    v6

--- Particle_tech/partech.py
+++ Particle_tech/partech.py
@@ -1,38 +1,43 @@
 '''
-This module can be used to perform typical Humidification calculations.
-Functions available in this module:
-- humid: Calculate the humidity using the partial pressure of the compound that can condense, the total pressure and the molar masses.
-                                                H = (pa / (Pt - pa)) * (MMa / MMb)                              (1)
-- Pa: Calculate the partial pressure of the compound using equation (1) and a inital guess pressure to solve for pressure.
-- Vh: Calculate the specific humid volume V'.
-                                                V = (1 / MMb + H / MMa) * ((R * T) / Pt)                        (2)
-- λ_water: Calulate the latent heat of vaporization of water. Temperature is in degrees C
-                                                λ_water = 2500 - 2.546 * T                                      (3)
-- λ_tol: Calulate the latent heat of vaporization of toluene. Temperature is in degrees C
-                                                λ_tol = 406.65 - 0.752 * T                                      (4)
-- enthalpy: Calculate the enthalpy of the system. Current version only solves for water and toluene and Cp values needs to be inserted manually.
-                                                enth = S * (T - Tref) + λ_w * H                                 (5)
-- percH: Calculate the percentage humidity. The ratio of the humidty to the saturated humidity.
-                                                %H = H / Ho                                                     (6)
-- relH: Calculate the relative humidity. The ratio of the partial pressure of the vapour to the saturated humidity.
-                                                Hr = pa/pa_sat                                                  (7)
-
-Nomenclature:
-Symbol      Name                                                    Units
-- pa        Partial pressure of component that will condense        (kPa)
-- Pt        Total pressure                                          (kPa)
-- MMa       Molar mass of component that will condense              (kg/kmol)
-- MMb       Molar mass of component that will condense              (kg/kmol)
-- H         Humidity                                                (kg/kg)
-- T         Temperature                                             (K)
-- pa_sat    Saturated pressure of component that will condense      (kPa)
-- dec       Decimal places                                          (-)
+This module can be used to perform typical calculations assossiated with Particle technology.
+Classes available in the module:
+    - Humidification
 
 This module works well in combination with the Antoine module.
 Created by Dewald Mare.
 
 '''
 class Humidification:
+    '''
+    This class can be used to perform typical Humidification calculations.
+    Functions available in this class:
+    - humid: Calculate the humidity using the partial pressure of the compound that can condense, the total pressure and the molar masses.
+                                                    H = (pa / (Pt - pa)) * (MMa / MMb)                              (1)
+    - Pa: Calculate the partial pressure of the compound using equation (1) and a inital guess pressure to solve for pressure.
+    - Vh: Calculate the specific humid volume V'.
+                                                    V = (1 / MMb + H / MMa) * ((R * T) / Pt)                        (2)
+    - λ_water: Calulate the latent heat of vaporization of water. Temperature is in degrees C
+                                                    λ_water = 2500 - 2.546 * T                                      (3)
+    - λ_tol: Calulate the latent heat of vaporization of toluene. Temperature is in degrees C
+                                                    λ_tol = 406.65 - 0.752 * T                                      (4)
+    - enthalpy: Calculate the enthalpy of the system. Current version only solves for water and toluene and Cp values needs to be inserted manually.
+                                                    enth = S * (T - Tref) + λ_w * H                                 (5)
+    - percH: Calculate the percentage humidity. The ratio of the humidty to the saturated humidity.
+                                                    %H = H / Ho                                                     (6)
+    - relH: Calculate the relative humidity. The ratio of the partial pressure of the vapour to the saturated humidity.
+                                                    Hr = pa/pa_sat                                                  (7)
+
+    Nomenclature:
+    Symbol      Name                                                    Units
+    - pa        Partial pressure of component that will condense        (kPa)
+    - Pt        Total pressure                                          (kPa)
+    - MMa       Molar mass of component that will condense              (kg/kmol)
+    - MMb       Molar mass of component that will condense              (kg/kmol)
+    - H         Humidity                                                (kg/kg)
+    - T         Temperature                                             (K)
+    - pa_sat    Saturated pressure of component that will condense      (kPa)
+    - dec       Decimal places                                          (-)
+    '''
     def __init__(self, *args):
         self.pa = pa
         self.Pt = Pt
