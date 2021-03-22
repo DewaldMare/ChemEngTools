ae44f14749bee1012122fd5af86df49d154609c2
DewaldMare <69504854+DewaldMare@users.noreply.github.com>
2021-03-22 08:57:44 +0200

    v4

--- Particle_tech/humidification.py
+++ Particle_tech/humidification.py
@@ -32,48 +32,85 @@ This module works well in combination with the Antoine module.
 Created by Dewald Mare.
 
 '''
+class Humidification:
+    def __init__(self, *args):
+        self.pa = pa
+        self.Pt = Pt
+        self.MMa = MMa
+        self.MMb = MMb
+        self.H = H
+        self.T = T
+        self.guessP = guess.P
+        self.name = name
+        self.Cpa = Cpa
+        self.Cpb = Cpb
+        self.dec = dec
+        self.psat = pasat
 
-def humid(pa, Pt, MMa, MMb):
-    H = (pa / (Pt - pa)) * (MMa / MMb)
-    return H
+    def humid(pa, Pt, MMa, MMb):
+        '''
+        Calculate the humidity using the partial pressure of the compound that can condense, the total pressure and the molar masses.
+                                                H = (pa / (Pt - pa)) * (MMa / MMb)
+        '''
+        H = (pa / (Pt - pa)) * (MMa / MMb)
+        return H
 
-def Pa(H, Pt, MMa, MMb, guessP):
-    from scipy.optimize import fsolve
-    def res(var):
-        P = var
-        res = H -  (P / (Pt - P)) * (MMa / MMb)
-        return res
-    guess = [guessP]
-    P = fsolve(res, guessP)
-    return P[0]
+    def Pa(H, Pt, MMa, MMb, guessP):
+        '''
+        Calculate the partial pressure of the compound using equation (1) and a inital guess pressure to solve for pressure.
+        '''
+        from scipy.optimize import fsolve
+        def res(var):
+            P = var
+            res = H -  (P / (Pt - P)) * (MMa / MMb)
+            return res
+        guess = [guessP]
+        P = fsolve(res, guessP)
+        return P[0]
 
-def Vh(MMa, MMb, H, Pt, T):
-    R = 8.3145 #kJ/kmolK
-    V = (1 / MMb + H / MMa) * ((R * T) / Pt)
-    return V
+    def Vh(MMa, MMb, H, Pt, T):
+        '''
+        Calculate the specific humid volume V'.
+                                                V = (1 / MMb + H / MMa) * ((R * T) / Pt)
+        '''
+        R = 8.3145 #kJ/kmolK
+        V = (1 / MMb + H / MMa) * ((R * T) / Pt)
+        return V
 
-λ_water = lambda T: 2500 - 2.546 * T #in degC
-λ_tol = lambda T: 406.65 - 0.752 * T #in degC
+    λ_water = lambda T: 2500 - 2.546 * T #in degC
+    λ_tol = lambda T: 406.65 - 0.752 * T #in degC
 
-def enthalpy(name, Cpa, Cpb, H, T):
-    if name == "Water":
-        λ = λ_water(T - 273.15)
-    elif name == "Toluene":
-        λ = λ_tol(T - 273.15)
-    S = Cpb + Cpa * H
-    Tref = 273.15 #K
-    enth = S * (T - Tref) + λ * H
-    return enth
+    def enthalpy(name, Cpa, Cpb, H, T):
+        '''
+        Calculate the enthalpy of the system. Current version only solves for water and toluene and Cp values needs to be inserted manually.
+                                                enth = S * (T - Tref) + λ_w * H
+        '''
+        if name == "Water":
+            λ = λ_water(T - 273.15)
+        elif name == "Toluene":
+            λ = λ_tol(T - 273.15)
+        S = Cpb + Cpa * H
+        Tref = 273.15 #K
+        enth = S * (T - Tref) + λ * H
+        return enth
 
-def percH(pa, psat, Pt, MMa, MMb, dec):
-    import numpy as np
-    H = humid(pa, Pt, MMa, MMb)
-    Ho = humid(psat, Pt, MMa, MMb)
-    val = H / Ho
-    perc = np.round(val, dec) * 100
-    return perc
+    def percH(pa, psat, Pt, MMa, MMb, dec):
+        '''
+        Calculate the percentage humidity. The ratio of the humidty to the saturated humidity.
+                                                %H = H / Ho
+        '''
+        import numpy as np
+        H = humid(pa, Pt, MMa, MMb)
+        Ho = humid(psat, Pt, MMa, MMb)
+        val = H / Ho
+        perc = np.round(val, dec) * 100
+        return perc
 
-def relH(pa, psat, dec):
-    Hr = pa / psat
-    perc = np.round(Hr, dec) * 100
-    return perc
+    def relH(pa, psat, dec):
+        '''
+        Calculate the relative humidity. The ratio of the partial pressure of the vapour to the saturated humidity.
+                                                Hr = pa/pa_sat
+        '''
+        Hr = pa / psat
+        perc = np.round(Hr, dec) * 100
+        return perc
